2a22f669f718a135295f514f9e3f22f0747b772d
vlad-duhota <vladarduino@gmail.com>
2023-04-22 15:40:52 +0300

    plugin

--- wp-content/plugins/woo_arta_shipping/class-shipping.php
+++ wp-content/plugins/woo_arta_shipping/class-shipping.php
@@ -5,6 +5,7 @@
 function start_arta_shipping_method() {
 	if ( ! class_exists( 'Arta_Shipping_Method' ) ) {
 		class Arta_Shipping_Method extends WC_Shipping_Method {
+
 			/**
 			 * Constructor for your shipping class
 			 *
@@ -17,7 +18,7 @@ function start_arta_shipping_method() {
 				$this->method_description = __( 'Shipping from:', 'woo_arta_shipping' ).' <a href="https://arta.io/" target="_blank">ARTA</a>.';
 
 				$this->init();
-				$this->enabled = isset( $this->settings['enabled'] ) ? $this->settings['enabled'] : 'yes';
+				//$this->enabled = isset( $this->settings['enabled'] ) ? $this->settings['enabled'] : 'yes';
 				$this->title   = isset( $this->settings['title']   ) ? $this->settings['title']   : __( 'ARTA Shipping', 'woo_arta_shipping' );
 			}
 
@@ -44,25 +45,18 @@ function start_arta_shipping_method() {
 
 				$this->form_fields = array(
 
-					'enabled' => array(
+					/*'enabled' => array(
 						'title'       => __( 'Enable', 'woo_arta_shipping' ),
 						'type'        => 'checkbox',
 						'description' => __( 'Enable this shipping.', 'woo_arta_shipping' ),
 						'default'     => 'yes'
-					),
+					),*/
 
 					'title' => array(
 						'title'       => __( 'Title', 'woo_arta_shipping' ),
 						'type'        => 'text',
 						'description' => __( 'Title to be display on site', 'woo_arta_shipping' ),
 						'default'     => __( 'ARTA Shipping', 'woo_arta_shipping' )
-					),
-
-					'weight' => array(
-						'title'       => __( 'Max Weight (kg)', 'woo_arta_shipping' ),
-						'type'        => 'number',
-						'description' => __( 'Maximum allowed weight', 'woo_arta_shipping' ),
-						'default'     => 100
 					),
 
 					'apikey' => array(
@@ -85,29 +79,27 @@ function start_arta_shipping_method() {
 			public function calculate_shipping( $package = array() ) {
 				foreach ( $package['contents'] as $item_id => $values ){
 					$_product         = $values['data'];
-
 					$width            = $_product->get_width();
 					$height           = $_product->get_height();
 					$length           = $_product->get_length();
 					$weight           = $_product->get_weight();
 					$product_quantity = $values['quantity'];
 					$product_price    = $_product->get_regular_price();
-					$product_country  = carbon_get_post_meta( $_product->get_id(), 'product_country' );
-					$product_postal   = carbon_get_post_meta( $_product->get_id(), 'product_postal' );
-					$product_region   = carbon_get_post_meta( $_product->get_id(), 'product_region' );
-					$product_city     = carbon_get_post_meta( $_product->get_id(), 'product_city' );
-					$product_address  = carbon_get_post_meta( $_product->get_id(), 'product_address' );
-
+					$product_country  = get_post_meta( $_product->get_id(), 'woo_arta_ship_product_country', true );
+					$product_postal   = get_post_meta( $_product->get_id(), 'woo_arta_ship_product_zip',  true );
+					$product_region   = '';//get_post_meta( $_product->get_id(), 'product_region',  true );
+					$product_city     = '';//get_post_meta( $_product->get_id(), 'product_city',    true );
+					$product_address  = '';//get_post_meta( $_product->get_id(), 'product_address', true );
 					$dest_country     = $package[ 'destination' ]['country'];
 					$dest_region      = $package[ 'destination' ]['state'];
 					$dest_city        = $package[ 'destination' ]['city'];
 					$dest_postal      = $package[ 'destination' ]['postcode'];
 					$dest_addr        = $package[ 'destination' ]['address'];
+					$key              = $this->settings['apikey'];
 
-					$arr = self::arta_request_cost( $width, $height, $length, $weight, $product_price, $product_quantity, $product_country, $product_region, $product_city, $product_postal, $product_address, $dest_country, $dest_region, $dest_city, $dest_postal, $dest_addr );
+					$arr = self::arta_request_cost( $key, $width, $height, $length, $weight, $product_price, $product_quantity, $product_country, $product_region, $product_city, $product_postal, $product_address, $dest_country, $dest_region, $dest_city, $dest_postal, $dest_addr );
 
-					//$txt = print_r( $arr, true );
-					//file_put_contents( '/var/www/arr_res.txt', $txt );
+					//file_put_contents( '/var/www/arr_res.txt', '<pre>'.print_r( $arr, true ).'</pre>' );
 
 					if( $arr['err'] ){
 						$this->add_rate( array(
@@ -127,10 +119,9 @@ function start_arta_shipping_method() {
 				}
 			}
 
-			public static function arta_request_cost( $product_width=0, $product_height=0, $length=0, $product_weight=0, $product_price=0, $quantity=1, $product_country='', $product_region='', $product_city='', $product_postal='', $product_address='', $dest_country='', $dest_region='', $dest_city='', $dest_postal='', $dest_addr='' ){
-				$res                  = array('err', 'data'=>array() );
-				$js_request           = '{"request":{"additional_services":["origin_condition_check"],"currency":"USD","destination":{"access_restrictions":["stairs_only"],"address_line_1":"x_destination_address_line_1","address_line_2":"x_destination_address_line_2","address_line_3":"x_destination_address_line_3","city":"x_destination_city","region":"x_destination_region","postal_code":"x_destination_postal_code","country":"x_destination_country","title":"x_destination_title","contacts":[{"name":"x_destination_contact_name","email_address":"x_destination_contact_email","phone_number":"x_destination_contact_phone"}]},"insurance":"arta_transit_insurance","internal_reference":"x_order_title","objects":[{"internal_reference":"x_objects_title","current_packing":["no_packing"],"depth":"3","details":{"materials":["canvas"],"creation_date":"x_objects_details_creation_date","creator":"x_objects_details_creator","notes":"x_objects_details_notes","title":"x_objects_details_title","is_fragile":false,"is_cites":false},"height":"x_objects_height","images":["x_objects_img"],"public_reference":"Round Smithson work","subtype":"painting_unframed","width":"x_objects_width","unit_of_measurement":"x_objects_dimension_unit","weight":"x_objects_weight_val","weight_unit":"x_objects_weight_unit","value":"x_objects_xvalue","value_currency":"x_objects_currency_xvalue"}],"origin":{"access_restrictions":["non_paved"],"address_line_1":"x_origin_address_line_1","address_line_2":"x_origin_address_line_2","address_line_3":"x_origin_address_line_3","city":"x_origin_city","region":"x_origin_region","postal_code":"x_origin_postal_code","country":"x_origin_country","title":"Warehouse","contacts":[{"name":"x_origin_contacts_name","email_address":"x_origin_contacts_email","phone_number":"x_origin_contacts_phone"}]},"preferred_quote_types":["parcel"],"public_reference":"Order #1437","shipping_notes":"New customer"}}';
-				$ARTA_Shipping_Method = new Arta_Shipping_Method();
+			public static function arta_request_cost( $key, $product_width=0, $product_height=0, $length=0, $product_weight=0, $product_price=0, $quantity=1, $product_country='', $product_region='', $product_city='', $product_postal='', $product_address='', $dest_country='', $dest_region='', $dest_city='', $dest_postal='', $dest_addr='' ){
+				$res                      = array('err'=>'', 'data'=>array() );
+				$js_request               = '{"request":{"additional_services":["origin_condition_check"],"currency":"USD","destination":{"access_restrictions":["stairs_only"],"address_line_1":"x_destination_address_line_1","address_line_2":"x_destination_address_line_2","address_line_3":"x_destination_address_line_3","city":"x_destination_city","region":"x_destination_region","postal_code":"x_destination_postal_code","country":"x_destination_country","title":"x_destination_title","contacts":[{"name":"x_destination_contact_name","email_address":"x_destination_contact_email","phone_number":"x_destination_contact_phone"}]},"insurance":"arta_transit_insurance","internal_reference":"x_order_title","objects":[{"internal_reference":"x_objects_title","current_packing":["no_packing"],"depth":"3","details":{"materials":["canvas"],"creation_date":"x_objects_details_creation_date","creator":"x_objects_details_creator","notes":"x_objects_details_notes","title":"x_objects_details_title","is_fragile":false,"is_cites":false},"height":"x_objects_height","images":["x_objects_img"],"public_reference":"Round Smithson work","subtype":"painting_unframed","width":"x_objects_width","unit_of_measurement":"x_objects_dimension_unit","weight":"x_objects_weight_val","weight_unit":"x_objects_weight_unit","value":"x_objects_xvalue","value_currency":"x_objects_currency_xvalue"}],"origin":{"access_restrictions":["non_paved"],"address_line_1":"x_origin_address_line_1","address_line_2":"x_origin_address_line_2","address_line_3":"x_origin_address_line_3","city":"x_origin_city","region":"x_origin_region","postal_code":"x_origin_postal_code","country":"x_origin_country","title":"Warehouse","contacts":[{"name":"x_origin_contacts_name","email_address":"x_origin_contacts_email","phone_number":"x_origin_contacts_phone"}]},"preferred_quote_types":["parcel"],"public_reference":"Order #1437","shipping_notes":"New customer"}}';
 
 				$url                      = 'https://api.arta.io/requests';
 				$product_width            = (float)$product_width;
@@ -150,7 +141,6 @@ function start_arta_shipping_method() {
 				$dest_postal              = (string)$dest_postal;
 				$dest_addr                = (string)$dest_addr;
 
-				$key                      = $ARTA_Shipping_Method->settings['apikey'];
 				$product_weight_unit      = substr( get_option('woocommerce_weight_unit'), 0, 2 );
 				$product_dimension_unit   = get_option('woocommerce_dimension_unit');
 				$product_currency_unit    = get_woocommerce_currency();
@@ -213,8 +203,10 @@ function start_arta_shipping_method() {
 					$data = json_decode( $response['body'], true );
 				}
 
-				//$txt = print_r( $data, true );
-				//file_put_contents( '/var/www/arr_data.txt', $txt );
+				//$txt1 = print_r( json_decode( $js_request ), true );
+				//$txt2 = print_r( $data, true );
+				//file_put_contents( '/var/www/arr_request.html', '<pre>'.$txt1.'</pre>' );
+				//file_put_contents( '/var/www/arr_response.html', '<pre>'.$txt2.'</pre>' );
 
 				if( isset( $data['disqualifications'] ) &&
 					count( $data['disqualifications'] ) > 0
@@ -230,7 +222,7 @@ function start_arta_shipping_method() {
 					}
 				}else{
 					foreach( $data['quotes'] as $quote_i ){
-						$res['data'][] = array( 'id'=>$quote_i['id'], 'quote_type'=>$quote_i['quote_type'],  'total'=>$quote_i['total'] );
+						$res['data'][] = array( 'id'=>$quote_i['id'], 'quote_type'=>$quote_i['quote_type'],  'total'=> ( $quote_i['total'] * $quantity ) );
 					}
 				}
 
--- wp-content/plugins/woo_arta_shipping/woo_arta_shipping.php
+++ wp-content/plugins/woo_arta_shipping/woo_arta_shipping.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://woo_arta_shipping/
  * Description: ARTA Shipping method for woocommerce.
  * Version: 1.0.0
- * Author: superpuperlesha
+ * Author: superpuperlesha@gmail.com
  * Author URI: https://superpuperlesha.com/
  * Text Domain: woo_arta_shipping
  * License: GPLv2
@@ -18,11 +18,86 @@ if ( ! defined( 'WPINC' ) ) {
 
 
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	require_once __DIR__.'/class-shipping.php';
+	require_once __DIR__ . '/class-shipping.php';
 }else{
 	add_action( 'admin_notices', function (){
 		echo'<div class="error">
                 <h3>'.__('Woocommerce plugin is required to use the plugin [woo_arta_shipping] !', 'woo_arta_shipping').'</h3>
              </div>';
 	} );
+}
+
+
+/*$ARTA_Shipping_Method = new Arta_Shipping_Method();
+$key                  = $ARTA_Shipping_Method->settings['apikey'];
+$product_width        = 30;
+$product_height       = 40;
+$length               = 30;
+$product_weight       = 5;
+$product_price        = 100;
+$quantity             = 1;
+$product_country      = 'US';
+$product_region       = '';
+$product_city         = '';
+$product_postal       = '10001';
+$product_address      = '';
+$dest_country         = 'US';
+$dest_region          = '';
+$dest_city            = '';
+$dest_postal          = '89714';
+$dest_addr            = '';
+$arta                 = Arta_Shipping_Method::arta_request_cost( $key, $product_width, $product_height, $length,
+	$product_weight, $product_price, $quantity, $product_country, $product_region, $product_city, $product_postal,
+	$product_address, $dest_country, $dest_region, $dest_city, $dest_postal, $dest_addr );
+echo '<pre>';
+print_r( $arta );
+echo '</pre>';*/
+
+
+/*
+ * Only 1 product in cart
+ */
+add_filter( 'woocommerce_add_cart_item_data', 'woo_arta_shipping_empty_cart', 10,  3);
+function woo_arta_shipping_empty_cart( $cart_item_data, $product_id, $variation_id ){
+	global $woocommerce;
+	$woocommerce->cart->empty_cart();
+	return $cart_item_data;
+}
+
+
+// Display admin product custom setting field(s)
+add_action('woocommerce_product_options_general_product_data', 'woo_arta_shipping_woocommerce_product_custom_fields');
+function woo_arta_shipping_woocommerce_product_custom_fields() {
+	global $product_object;
+
+	//echo '<div class="product_custom_field ">';
+	woocommerce_wp_text_input( array(
+		'id'          => 'woo_arta_ship_product_country',
+		'label'       => __('Arta Country:', 'woocommerce' ),
+		'placeholder' => 'US',
+		'desc_tip'    => 'true',
+		'description' => __('Arta Country format: US', 'woocommerce' ),
+		'value'       => get_post_meta( $product_object->get_id(), 'woo_arta_ship_product_country', true ),
+	) );
+
+	woocommerce_wp_text_input( array(
+		'id'          => 'woo_arta_ship_product_zip',
+		'label'       => __('Arta ZIP:', 'woocommerce' ),
+		'placeholder' => 'US',
+		'desc_tip'    => 'true',
+		'description' => __('Arta ZIP format: 10001', 'woocommerce' ),
+		'value'       => get_post_meta( $product_object->get_id(), 'woo_arta_ship_product_zip', true ),
+	) );
+	//echo '</div>';
+}
+
+// Save admin product custom setting field(s) values
+add_action('woocommerce_admin_process_product_object', 'woo_arta_shipping_woocommerce_product_custom_fields_save');
+function  woo_arta_shipping_woocommerce_product_custom_fields_save( $product ) {
+	if ( isset( $_POST['woo_arta_ship_product_country'] ) ) {
+		update_post_meta( $product->get_id(), 'woo_arta_ship_product_country', htmlspecialchars( $_POST['woo_arta_ship_product_country'] ) );
+	}
+	if ( isset( $_POST['woo_arta_ship_product_zip'] ) ) {
+		update_post_meta( $product->get_id(), 'woo_arta_ship_product_zip', htmlspecialchars( $_POST['woo_arta_ship_product_zip'] ) );
+	}
 }
